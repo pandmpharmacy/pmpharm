7c2d01aecbd10c1afa154c0295ff8aca668374e2
Kaleem Baksh <ae86.89@gmail.com>
2023-09-18 16:33:04 -0500

    added some pet compounding text. need help on positioning code

--- src/FooterComponent.js
+++ src/FooterComponent.js
@@ -12,6 +12,9 @@ const navigationItems = [
 ];
 
 function FooterComponent() {
+  const address = "7753 Lake Worth Rd Lake Worth, FL 33467";
+  const mapLink = `https://www.google.com/maps?q=${encodeURIComponent(address)}`;
+
   return (
     <footer className="footer-container">
       <div className="footer-column">
@@ -20,9 +23,9 @@ function FooterComponent() {
 
       <div className="footer-column">
         <h3>Contact Us</h3>
-        <p>Address: 7753 Lake Worth Rd Lake Worth, FL 33467</p>
-        <p>Phone: (561) 660-8650</p>
-        <p>Email: support@pandmpharmacy.com</p>
+        <p><a href={mapLink} target="_blank" rel="noopener noreferrer">7753 Lake Worth Rd Lake Worth, FL 33467</a></p>
+        <p><a href="tel:+15616608650">(561) 660-8650</a></p>
+        <p><a href="mailto:support@pandmpharmacy.com">support@pandmpharmacy.com</a></p>
       </div>
 
       <div className="footer-column">
@@ -44,7 +47,7 @@ function FooterComponent() {
       </div>
 
       <div className="copyright">
-        &copy; {new Date().getFullYear()} P & M Pharmacy. All rights reserved.
+        &copy; {new Date().getFullYear()} P&M Pharmacy. All rights reserved.
       </div>
     </footer>
   );
--- src/PetCompunding.js
+++ src/PetCompunding.js
@@ -1,10 +1,18 @@
 import React from "react";
 import LandingHeader from "./LandingHeader";
+import LeftInterlockedTextBoxes from "./LeftInterlockedTextBoxes";
+import RightInterlockedTextBoxes from "./RightInterlockedTextBoxes";
 
 function PetCompunding() {
   return (
     <div>
       <LandingHeader title={"PetCompounding"} />
+      <LeftInterlockedTextBoxes title="Veterinary Medications" message="Your Trusted Veterinary Compounding Pharmacy" />
+        <RightInterlockedTextBoxes title="Flavored Medication" message="Pets can be picky about flavors, making medication administration a challenge. At P&M Pharmacy, our skilled pet compounding pharmacy prepares delicious flavored medications that your pet will love. From tuna for cats to tutti frutti for exotic pets, we make giving medication enjoyable and stress-free."/>
+        <LeftInterlockedTextBoxes title="Ideal Size, Strength, and Dosage" message="Tailored Medications for Your Furry Friend" />
+        <RightInterlockedTextBoxes title="Transdermal Medication" message="Some pets need stress-free medication options. P&M Pharmacy offers transdermal medications that are ideal for animals with hypertension or cardiovascular issues. This convenient delivery method makes medication administration easier for both you and your pet. We also provide topical medications for localized treatment, reducing discomfort."/>
+        <LeftInterlockedTextBoxes title="Unavailable Medication?" message="Customized Solutions for Unavailable Medications" />
+        <RightInterlockedTextBoxes title="Combination Needed?" message="Managing multiple medications can be expensive and inconvenient. P&M Pharmacy simplifies treatment by compounding compatible medications into a single, easy-to-administer arrangement. We also offer various dosage forms, including medicated treats, containing multiple drugs, making pet care more convenient for you."/>
     </div>
   );
 }
