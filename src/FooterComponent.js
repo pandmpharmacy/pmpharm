bcef1ba4ca2f89bac3ed2804cad657038699fe94
Walid Younis <walid.younis.25@gmail.com>
2023-09-18 21:01:04 -0500

    updated copywright

--- src/FooterComponent.js
+++ src/FooterComponent.js
@@ -13,43 +13,56 @@ const navigationItems = [
 
 function FooterComponent() {
   const address = "7753 Lake Worth Rd Lake Worth, FL 33467";
-  const mapLink = `https://www.google.com/maps?q=${encodeURIComponent(address)}`;
+  const mapLink = `https://www.google.com/maps?q=${encodeURIComponent(
+    address
+  )}`;
 
   return (
-    <footer className="footer-container">
-      <div className="footer-column">
-        <img src={logo} alt="logo" className="footer-logo" />
-      </div>
+    <>
+      <footer className="footer-container">
+        <div className="footer-column">
+          <img src={logo} alt="logo" className="footer-logo" />
+        </div>
 
-      <div className="footer-column">
-        <h3>Contact Us</h3>
-        <p><a href={mapLink} target="_blank" rel="noopener noreferrer">7753 Lake Worth Rd Lake Worth, FL 33467</a></p>
-        <p><a href="tel:+15616608650">(561) 660-8650</a></p>
-        <p><a href="mailto:support@pandmpharmacy.com">support@pandmpharmacy.com</a></p>
-      </div>
+        <div className="footer-column">
+          <h3>Contact Us</h3>
+          <p>
+            <a href={mapLink} target="_blank" rel="noopener noreferrer">
+              7753 Lake Worth Rd Lake Worth, FL 33467
+            </a>
+          </p>
+          <p>
+            <a href="tel:+15616608650">(561) 660-8650</a>
+          </p>
+          <p>
+            <a href="mailto:support@pandmpharmacy.com">
+              support@pandmpharmacy.com
+            </a>
+          </p>
+        </div>
 
-      <div className="footer-column">
-        <h3>About Us</h3>
-        <p>
-          At P & M Pharmacy, we prioritize your health. With years of
-          experience and a dedicated team, we ensure you receive the best
-          medications available. Trust us for all your pharmaceutical needs.
-        </p>
-      </div>
+        <div className="footer-column">
+          <h3>About Us</h3>
+          <p>
+            At P & M Pharmacy, we prioritize your health. With years of
+            experience and a dedicated team, we ensure you receive the best
+            medications available. Trust us for all your pharmaceutical needs.
+          </p>
+        </div>
 
-      <div className="footer-column">
-        <h3>Navigation</h3>
-        <ul className="footer-nav-list">
-          {navigationItems.map((navItem, index) => (
-            <li key={index}>{navItem}</li>
-          ))}
-        </ul>
-      </div>
-
+        <div className="footer-column">
+          <h3>Navigation</h3>
+          <ul className="footer-nav-list">
+            {navigationItems.map((navItem, index) => (
+              <li key={index}>{navItem}</li>
+            ))}
+          </ul>
+        </div>
+      </footer>
       <div className="copyright">
         &copy; {new Date().getFullYear()} P&M Pharmacy. All rights reserved.
       </div>
-    </footer>
+    </>
   );
 }
 
