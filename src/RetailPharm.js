42f86f1ffcd1ff59fcc514183015416f3e5aa2a9
Kaleem Baksh <ae86.89@gmail.com>
2023-09-21 23:45:09 -0500

    added more boxes to retail

--- src/RetailPharm.js
+++ src/RetailPharm.js
@@ -34,6 +34,31 @@ function RetailPharm() {
       title: "Box 5 Title",
       text: "Box 5 Description",
     },
+    {
+      imageSrc: "image6.jpg",
+      title: "Box 6 Title",
+      text: "Box 6 Description",
+    },
+    {
+      imageSrc: "image7.jpg",
+      title: "Box 7 Title",
+      text: "Box 7 Description",
+    },
+    {
+      imageSrc: "image8.jpg",
+      title: "Box 8 Title",
+      text: "Box 8 Description",
+    },
+    {
+      imageSrc: "image9.jpg",
+      title: "Box 9 Title",
+      text: "Box 9 Description",
+    },
+    {
+      imageSrc: "image10.jpg",
+      title: "Box 10 Title",
+      text: "Box 10 Description",
+    }
     // Add more data for additional boxes if needed
   ];
 
@@ -68,6 +93,31 @@ function RetailPharm() {
           <img src={pairedBoxData[4].imageSrc} alt="Box 5" />
           <h2>{pairedBoxData[4].title}</h2>
           <p>{pairedBoxData[4].text}</p>
+        </div>
+        <div className="box" style={{ gridArea: "box6" }}>
+          <img src={pairedBoxData[5].imageSrc} alt="Box 6" />
+          <h2>{pairedBoxData[5].title}</h2>
+          <p>{pairedBoxData[5].text}</p>
+        </div>
+        <div className="box" style={{ gridArea: "box7" }}>
+          <img src={pairedBoxData[6].imageSrc} alt="Box 7" />
+          <h2>{pairedBoxData[6].title}</h2>
+          <p>{pairedBoxData[6].text}</p>
+        </div>
+        <div className="box" style={{ gridArea: "box8" }}>
+          <img src={pairedBoxData[7].imageSrc} alt="Box 8" />
+          <h2>{pairedBoxData[7].title}</h2>
+          <p>{pairedBoxData[7].text}</p>
+        </div>
+        <div className="box" style={{ gridArea: "box9" }}>
+          <img src={pairedBoxData[8].imageSrc} alt="Box 9" />
+          <h2>{pairedBoxData[8].title}</h2>
+          <p>{pairedBoxData[8].text}</p>
+        </div>
+        <div className="box" style={{ gridArea: "box10" }}>
+          <img src={pairedBoxData[9].imageSrc} alt="Box 10" />
+          <h2>{pairedBoxData[9].title}</h2>
+          <p>{pairedBoxData[9].text}</p>
         </div>
       </div>
     </div>
