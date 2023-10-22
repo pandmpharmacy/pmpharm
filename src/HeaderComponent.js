688f785250ba2f034edaaa0176cb92f42f334f16
Walid Younis <walid.younis.25@gmail.com>
2023-10-21 23:21:02 -0500

    updated header

--- src/HeaderComponent.js
+++ src/HeaderComponent.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import logo from "../src/images/logo.webp";
 import "./HeaderComponent.css";
 import logoWebp from "../src/images/logo.webp";
@@ -8,55 +8,16 @@ function HeaderComponent() {
   const navigate = useNavigate(); // Hook to get the navigate function
 
   const [showMobileMenu, setShowMobileMenu] = useState(false);
-  const [hasScrolled, setHasScrolled] = useState(false);
-  function calculateScrollPercentage() {
-    const scrollY = window.scrollY;
-    const viewportHeight = window.innerHeight;
-    const documentHeight = document.documentElement.scrollHeight;
 
-    return scrollY / (documentHeight - viewportHeight);
-  }
-  const MAX_MARGIN_LEFT = -25; // This is a sample value. You can adjust based on your layout.
   const handleNavigation = (path) => {
     navigate(path);
   };
-  window.addEventListener("scroll", () => {
-    // Get the logo and its div
-    const logo = document.querySelector(".logo");
-    const logoDiv = document.querySelector(".logo_div");
 
-    // Calculate the proportion of how much the page has been scrolled
-    const scrollPercentage = calculateScrollPercentage();
-
-    // Change the max-width of the logo based on scroll percentage
-    const maxWidth = 80 - 70 * scrollPercentage; // linearly decrease to 30px
-    logo.style.maxWidth = `${Math.max(maxWidth, 80)}px`; // Ensure it doesn't go below 30px
-    const marginLeft = MAX_MARGIN_LEFT * scrollPercentage;
-    logoDiv.style.marginLeft = `${Math.max(marginLeft, MAX_MARGIN_LEFT)}%`; // Use max to ensure it doesn't exceed MAX_MARGIN_LEFT
-    // Move the logo_div to the left based on scroll percentage
-  });
   const toggleMobileMenu = () => {
     setShowMobileMenu(!showMobileMenu);
   };
 
-  useEffect(() => {
-    // Function to handle scroll event
-    const handleScroll = () => {
-      if (window.scrollY > 0) {
-        setHasScrolled(true);
-      } else {
-        setHasScrolled(false);
-      }
-    };
 
-    // Add scroll event listener when the component mounts
-    window.addEventListener("scroll", handleScroll);
-
-    // Remove the event listener when the component unmounts
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
   document.addEventListener("DOMContentLoaded", function () {
     const menuToggle = document.querySelector(".menu-toggle");
     const navList = document.querySelector(".nav-list");
@@ -69,14 +30,13 @@ function HeaderComponent() {
   });
 
   // Define the CSS class for the header based on hasScrolled
-  const headerClass = `header_wrapper ${hasScrolled ? "with-shadow" : ""}`;
 
   // Define the homepage URL
   const homepageUrl = "/"; // Replace with your actual homepage URL
 
   return (
     <>
-      <div className={headerClass}>
+      <div className="header_wrapper">
         <div className="logo_div">
           <a href={homepageUrl}>
             {" "}
