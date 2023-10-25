e7c5c736c4f744ab15e3a353437516b3adb95ba0
Kaleem Baksh <ae86.89@gmail.com>
2023-10-25 16:47:27 -0500

    removed useNavigate from prayer times

--- src/PrayerTimesTest.js
+++ src/PrayerTimesTest.js
@@ -1,6 +1,6 @@
 //import React from "react";
 import "./PrayerTimesTest.css";
-import React, { useState, useEffect, useNavigate } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function PrayerTimes() {
   const [prayerData, setPrayerData] = useState({
